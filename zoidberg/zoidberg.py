5b0baa8e7184d6811e599ea1b194b6808da96868
David Schwörer <dave@ipp.mpg.de>
2020-09-23 13:59:45 +0200

    Don't save Bxy twice
    reverts part of a6216b78e7

--- zoidberg/zoidberg.py
+++ zoidberg/zoidberg.py
@@ -281,7 +281,6 @@ def write_maps(grid, magnetic_field, maps, gridfile='fci.grid.nc',
 
         # Magnetic field
         f.write("B", Bmag)
-        f.write("Bxy", Bmag)
 
         # Pressure
         f.write("pressure", pressure)
