7135d14e246be75fbafa5a9bb5ea540139b3fcf5
Peter Hill <peter.hill@york.ac.uk>
2021-12-13 09:17:29 +0000

    Ensure `__version__` is set correctly

--- zoidberg/__init__.py
+++ zoidberg/__init__.py
@@ -1,9 +1,21 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except (ModuleNotFoundError, ImportError):
+    from importlib_metadata import version, PackageNotFoundError
+try:
+    __version__ = version("zoidberg")
+except PackageNotFoundError:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
 import zoidberg
 
 from . import field, fieldtracer, grid, plot
 from .zoidberg import make_maps, write_maps
 
 __all__ = [
+    "__version__",
     "field",
     "fieldtracer",
     "grid",
