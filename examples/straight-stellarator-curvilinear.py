6fbf7f02c89c6f50fe37ab0e7fe8d141f044ba74
Peter Hill <peter.hill@york.ac.uk>
2024-01-18 17:19:55 +0000

    Apply black to examples

--- examples/straight-stellarator-curvilinear.py
+++ examples/straight-stellarator-curvilinear.py
@@ -9,11 +9,13 @@ import zoidberg
 
 #############################################################################
 # Define the magnetic field
-    
-# Length in y after which the coils return to their starting (R,Z) locations
-yperiod = 10.
 
-magnetic_field = zoidberg.field.StraightStellarator(I_coil=0.3, radius = 1.0, yperiod = yperiod)
+# Length in y after which the coils return to their starting (R,Z) locations
+yperiod = 10.0
+
+magnetic_field = zoidberg.field.StraightStellarator(
+    I_coil=0.3, radius=1.0, yperiod=yperiod
+)
 
 #############################################################################
 # Create the inner flux surface, starting at a point at phi=0
@@ -27,19 +29,20 @@ nslices = 8  # Number of poloidal slices
 ycoords = np.linspace(0, yperiod, nslices)
 npoints = 20  # Points per poloidal slice
 
-rzcoord, ycoords = zoidberg.fieldtracer.trace_poincare(magnetic_field, start_r, start_z, yperiod,
-                                                     y_slices=ycoords, revs=npoints)
-    
+rzcoord, ycoords = zoidberg.fieldtracer.trace_poincare(
+    magnetic_field, start_r, start_z, yperiod, y_slices=ycoords, revs=npoints
+)
+
 inner_lines = []
 for i in range(nslices):
-    r = rzcoord[:,i,0]
-    z = rzcoord[:,i,1]
-    line = zoidberg.rzline.line_from_points(r,z)
+    r = rzcoord[:, i, 0]
+    z = rzcoord[:, i, 1]
+    line = zoidberg.rzline.line_from_points(r, z)
     # Re-map the points so they're approximately uniform in distance along the surface
     # Note that this results in some motion of the line
     line = line.equallySpaced()
     inner_lines.append(line)
-    
+
 # Now have a list of y coordinates (ycoords) and inner lines (inner_lines)
 
 #############################################################################
@@ -54,12 +57,15 @@ outer_line = zoidberg.rzline.circle(R0=0.0, r=0.8)
 nx = 20
 ny = 20
 
-pol_grids = [ zoidberg.poloidal_grid.grid_elliptic(inner_line, outer_line, nx,ny) for inner_line in inner_lines ]
+pol_grids = [
+    zoidberg.poloidal_grid.grid_elliptic(inner_line, outer_line, nx, ny)
+    for inner_line in inner_lines
+]
 
 #############################################################################
 # Create a grid, then calculate forward and backward maps
 
-grid = zoidberg.grid.Grid( pol_grids, ycoords, yperiod, yperiodic=True)
+grid = zoidberg.grid.Grid(pol_grids, ycoords, yperiod, yperiodic=True)
 
 maps = zoidberg.make_maps(grid, magnetic_field)
 
@@ -69,7 +75,9 @@ maps = zoidberg.make_maps(grid, magnetic_field)
 filename = "stellarator.fci.nc"
 
 print("Writing to grid file '{0}'".format(filename))
-zoidberg.write_maps(grid, magnetic_field, maps, gridfile=filename, new_names=False, metric2d=True)
+zoidberg.write_maps(
+    grid, magnetic_field, maps, gridfile=filename, new_names=False, metric2d=True
+)
 
 #############################################################################
 # Plot maps
--- examples/tokamak.py
+++ examples/tokamak.py
@@ -2,15 +2,19 @@
 
 import numpy as np
 import zoidberg
-   
-field = zoidberg.field.GEQDSK("test_1.geqdsk") # Read magnetic field
 
-grid = zoidberg.grid.rectangular_grid(100, 10, 100,
-                                      1.5-0.1, # Range in R (max - min)
-                                      2*np.pi, # Toroidal angle
-                                      3., # Range in Z
-                                      xcentre=(1.5+0.1)/2, # Middle of grid in R
-                                      yperiodic=True) # Periodic in toroidal angle
+field = zoidberg.field.GEQDSK("test_1.geqdsk")  # Read magnetic field
+
+grid = zoidberg.grid.rectangular_grid(
+    100,
+    10,
+    100,
+    1.5 - 0.1,  # Range in R (max - min)
+    2 * np.pi,  # Toroidal angle
+    3.0,  # Range in Z
+    xcentre=(1.5 + 0.1) / 2,  # Middle of grid in R
+    yperiodic=True,
+)  # Periodic in toroidal angle
 
 # Create the forward and backward maps
 maps = zoidberg.make_maps(grid, field)
@@ -22,4 +26,3 @@ zoidberg.write_maps(grid, field, maps, gridfile="tokamak.fci.nc")
 # Plot maps
 
 zoidberg.plot.plot_forward_map(grid, maps)
-
