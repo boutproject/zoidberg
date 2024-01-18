36aa52abeff34e7f211a1174f9c315fc2aea2d81
Peter Hill <peter.hill@york.ac.uk>
2024-01-18 17:19:55 +0000

    Apply black to examples

--- examples/field_line.py
+++ examples/field_line.py
@@ -5,14 +5,18 @@
 import zoidberg
 
 # Size of the domain in y (periodic)
-yperiod = 10.
+yperiod = 10.0
 
 # Define magnetic field
-magnetic_field = zoidberg.field.StraightStellarator(I_coil=0.4, radius = 1.0, yperiod = yperiod)
+magnetic_field = zoidberg.field.StraightStellarator(
+    I_coil=0.4, radius=1.0, yperiod=yperiod
+)
 
 # Make 3D plot
-zoidberg.plot.plot_3d_field_line(magnetic_field, 
-                                 0.3,                    # x starting position
-                                 0.0,                    # z starting position
-                                 yperiod,                # Periodicity of y domain
-                                 cycles = 20)
+zoidberg.plot.plot_3d_field_line(
+    magnetic_field,
+    0.3,  # x starting position
+    0.0,  # z starting position
+    yperiod,  # Periodicity of y domain
+    cycles=20,
+)
--- examples/straight-stellarator-poincare.py
+++ examples/straight-stellarator-poincare.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Create grids for a straight stellarator, based on 
+# Create grids for a straight stellarator, based on
 # curvilinear grids
 
 import numpy as np
@@ -10,10 +10,12 @@ import zoidberg
 
 #############################################################################
 # Define the magnetic field
-    
-# Length in y after which the coils return to their starting (R,Z) locations
-yperiod = 10.
 
-magnetic_field = zoidberg.field.StraightStellarator(I_coil=0.4, radius = 1.0, yperiod = yperiod)
+# Length in y after which the coils return to their starting (R,Z) locations
+yperiod = 10.0
+
+magnetic_field = zoidberg.field.StraightStellarator(
+    I_coil=0.4, radius=1.0, yperiod=yperiod
+)
 
 zoidberg.plot.plot_poincare(magnetic_field, 0.3, 0.0, yperiod)
