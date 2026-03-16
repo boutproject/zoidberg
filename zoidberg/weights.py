bffb46b7f1080192a019f61b3f9f7c52f74eb405
Ben Dudson <dudson2@llnl.gov>
2026-03-16 16:53:15 -0700

    Weights: Add rows in Y boundaries
    Needed to enable correct calculation of divergence operator.

--- zoidberg/weights.py
+++ zoidberg/weights.py
@@ -101,16 +101,16 @@ def calc_cell_numbers(maps):
     for i in range(MXG, nx - MXG):
         for j in range(ny):
             for k in range(nz):
-                if backward_xt_prime[i, j, k] < 0.0:
+                if backward_xt_prime[i, j, k] < MXG:
                     backward_cell_number[i, j, k] = cell_number
                     cell_number += 1
-                elif backward_xt_prime[i, j, k] >= nx:
+                elif backward_xt_prime[i, j, k] > nx - MXG - 1:
                     backward_cell_number[i, j, k] = cell_number
                     cell_number += 1
-                if forward_xt_prime[i, j, k] < 0.0:
+                if forward_xt_prime[i, j, k] < MXG:
                     forward_cell_number[i, j, k] = cell_number
                     cell_number += 1
-                elif forward_xt_prime[i, j, k] >= nx:
+                elif forward_xt_prime[i, j, k] > nx - MXG - 1:
                     forward_cell_number[i, j, k] = cell_number
                     cell_number += 1
 
@@ -123,13 +123,16 @@ def calc_cell_numbers(maps):
     }
 
 
-def calc_interpolation(cell_number, MXG, yoffset, xtarr, ztarr, bndry_cell_number):
+def calc_interpolation(numbering, maps):
     """
     Calculate CSR format matrix representing a 2D (X-Z) interpolation operation
 
     Implements the cubic Catmull-Rom spline
     Coefficients taken from https://en.wikipedia.org/wiki/Cubic_Hermite_spline
     """
+
+    cell_number = numbering["cell_number"]
+    MXG = maps["MXG"]
 
     # Offsets and weights for 1D interpolation
     offsets1D = [-1, 0, 1, 2]
@@ -144,53 +147,122 @@ def calc_interpolation(cell_number, MXG, yoffset, xtarr, ztarr, bndry_cell_numbe
             ]
         )
 
-    # CSR format
+    # At the end the operators will be stored in CSR
+    # format. When assembling the rows are not set in order
+    # so a more flexible format is needed.
+    # To use: Append (col, weight) pairs to the row lists.
+    class Matrix:
+        def __init__(self, N):
+            self.rows = [[] for _ in range(N)]
 
-    weights = []
-    columns = []
-    rows = []
+        def csr(self):
+            """Calculates CSR format arrays"""
+            rows = np.full(len(self.rows), -1, dtype=int)
+            weights = []
+            columns = []
+            for i, entries in enumerate(self.rows):
+                if len(entries) == 0:
+                    continue
+                rows[i] = len(weights)
+                for col, weight in entries:
+                    columns.append(col)
+                    weights.append(weight)
+            return {"weights": weights, "columns": columns, "rows": rows}
+
+    forward_mat = Matrix(numbering["N_cells"])
+    backward_mat = Matrix(numbering["N_cells"])
 
     nx, ny, nz = cell_number.shape
 
-    weight_number = 0  # Track location in weights & columns arrays
-    for i in range(MXG, nx - MXG):
-        for j in range(ny):
-            for k in range(nz):
-                xt = xtarr[i, j, k]
-                zt = ztarr[i, j, k]
-                if (xt < 0.0) or (xt >= nx):
-                    # Boundary => Take value from yup/ydown boundary cell
-                    rows.append(weight_number)
-                    columns.append(bndry_cell_number[i, j, k])
-                    weights.append(1.0)
-                    weight_number += 1
-                else:
-                    # Not a boundary point => Interpolating
-                    rows.append(weight_number)
-                    xi = int(xt)  # Floor
-                    zi = int(zt)
+    # Calculate first forward interpolation coefficients then backward
+    #
+    # This looks complicated because the rows corresponding to boundary
+    # cells in the forward direction are set using the backward mapping.
+    #
+    # Rows in both forward_mat and backward_mat are set from
+    # bndry_cell_number
+    def calc(mat, rev_mat, yoffset, xtarr, ztarr, bndry_cell_number):
+        for i in range(MXG, nx - MXG):
+            for j in range(ny):
+                for k in range(nz):
+                    row = cell_number[i, j, k]
+                    assert row >= 0
 
-                    weights_x = weights1D(xt - xi)
-                    weights_z = weights1D(zt - zi)
+                    xt = xtarr[i, j, k]
+                    zt = ztarr[i, j, k]
+                    if (xt < MXG) or (xt > nx - MXG - 1):
+                        # Boundary => Take value from yup/ydown boundary cell
+                        bndry_row = bndry_cell_number[i, j, k]
+                        assert bndry_row >= 0
+                        mat.rows[row] = [(bndry_row, 1.0)]
 
-                    # Weights should sum to 1
-                    assert abs(np.sum(weights_x) - 1) < 1e-8
-                    assert abs(np.sum(weights_z) - 1) < 1e-8
+                        # Boundary cells "free" boundary extrapolate
+                        mat.rows[bndry_row] = [(row, -1.0), (bndry_row, 2.0)]
 
-                    for xo, xw in zip(offsets1D, weights_x):
-                        for zo, zw in zip(offsets1D, weights_z):
-                            col = cell_number[
+                        # In the reverse matrix e.g. going forward
+                        # from the backward boundary cell. This is
+                        # included because the divergence operator is
+                        # the transpose of the gradient.  If the
+                        # divergence in an evolving cell depends on a
+                        # boundary cell, then the gradient in the
+                        # boundary cell must depend on the evolving
+                        # cell.
+                        rev_mat.rows[bndry_row] = [(row, 1.0)]
+                    else:
+                        # Not a boundary point => Interpolating
+                        xi = int(xt)  # Floor
+                        zi = int(zt)
+
+                        weights_x = weights1D(xt - xi)
+                        weights_z = weights1D(zt - zi)
+
+                        # Ensure that points in the X boundary are not used
+                        for wi in range(len(offsets1D)):
+                            if xi + offsets1D[wi] < MXG:
+                                # Move weight away from boundary
+                                weights_x[wi + 1] += weights_x[wi]
+                                weights_x[wi] = 0.0
+                        for wi in reversed(range(len(offsets1D))):
+                            if xi + offsets1D[wi] >= nx - MXG:
+                                weights_x[wi - 1] += weights_x[wi]
+                                weights_x[wi] = 0.0
+
+                        # Weights should sum to 1
+                        assert abs(np.sum(weights_x) - 1) < 1e-8
+                        assert abs(np.sum(weights_z) - 1) < 1e-8
+
+                        for xo, xw in zip(offsets1D, weights_x):
+                            if abs(xw) < 1e-10:
+                                continue
+                            for zo, zw in zip(offsets1D, weights_z):
+                                col = cell_number[
                                     np.clip(xi + xo, 0, nx - 1),
                                     (j + yoffset + ny) % ny,
                                     (zi + zo + nz) % nz,
-                            ]
-                            assert col >= 0
-                            columns.append(col)
-                            weights.append(xw * zw)
-                            weight_number += 1
-    assert weight_number == len(weights)
-    assert len(weights) == len(columns)
-    return {"weights": weights, "columns": columns, "rows": rows}
+                                ]
+                                assert col >= 0
+
+                                mat.rows[row].append((col, xw * zw))
+
+    calc(
+        forward_mat,
+        backward_mat,
+        +1,
+        maps["forward_xt_prime"],
+        maps["forward_zt_prime"],
+        numbering["forward_cell_number"],
+    )
+
+    calc(
+        backward_mat,
+        forward_mat,
+        -1,
+        maps["backward_xt_prime"],
+        maps["backward_zt_prime"],
+        numbering["backward_cell_number"],
+    )
+
+    return forward_mat.csr(), backward_mat.csr()
 
 
 def calc_weights(maps):
@@ -202,23 +274,7 @@ def calc_weights(maps):
 
     numbering = calc_cell_numbers(maps)
 
-    forward = calc_interpolation(
-        numbering["cell_number"],
-        maps["MXG"],
-        +1,
-        maps["forward_xt_prime"],
-        maps["forward_zt_prime"],
-        numbering["forward_cell_number"]
-    )
-
-    backward = calc_interpolation(
-        numbering["cell_number"],
-        maps["MXG"],
-        -1,
-        maps["backward_xt_prime"],
-        maps["backward_zt_prime"],
-        numbering["backward_cell_number"]
-    )
+    forward, backward = calc_interpolation(numbering, maps)
 
     return {
         "cell_number": numbering["cell_number"],
