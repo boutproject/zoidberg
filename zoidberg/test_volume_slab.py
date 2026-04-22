248612ddf701a1cec02a0ffe6df53011233b13ce
Tobias Tork <toto@fc-ubu3-066.ipp-hgw.mpg.de>
2026-04-22 16:39:11 +0200

    Add pytest functionality of the test

--- zoidberg/test_volume_slab.py
+++ zoidberg/test_volume_slab.py
@@ -4,6 +4,8 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
+
+
 from zoidberg import field as zbfield
 from zoidberg import grid as zbgrid
 from zoidberg import poloidal_grid, zoidberg
@@ -18,7 +20,7 @@ def polyfunc(y, p):
     return result
 
 
-class testfield(zbfield.MagneticField):
+class fld(zbfield.MagneticField):
     def __init__(self, polynomials=[1.0]):
         """
         Parameters
@@ -44,127 +46,127 @@ class testfield(zbfield.MagneticField):
 
 
 # %% Create a slab grid and calculate the volumes of the cells
-field = testfield([1.0, 0.3, -0.5, 0.2])
-nx = 20
-ny = 8
-nz = 20
-pol_grids = []
-for i in range(ny):
-    pol_grids.append(
-        poloidal_grid.RectangularPoloidalGrid(
-            nx=nx, nz=nz, Lx=0.05, Lz=0.05, Rcentre=5.0
+
+def test_slab_volume():
+    
+    field = fld([1.0, 0.3, -0.5, 0.2])
+    nx = 20
+    ny = 8
+    nz = 20
+    pol_grids = []
+    for i in range(ny):
+        pol_grids.append(
+            poloidal_grid.RectangularPoloidalGrid(
+                nx=nx, nz=nz, Lx=0.05, Lz=0.05, Rcentre=5.0
+            )
         )
+    
+    grid = zbgrid.Grid(
+        pol_grids,
+        np.linspace(0.0, 2.0 * np.pi, ny, endpoint=False),
+        2.0 * np.pi,
+        yperiodic=True,
     )
-
-grid = zbgrid.Grid(
-    pol_grids,
-    np.linspace(0.0, 2.0 * np.pi, ny, endpoint=False),
-    2.0 * np.pi,
-    yperiodic=True,
-)
-reffactors = [1, 5, 10, 20, 50]
-
-maps = {}
-for ref in reffactors:
-    maps[str(ref)] = zoidberg.make_maps(grid, field, refine_parallel_integral=ref)
-
-
-fn = f"testslabgrid_{nx}_{ny}_{nz}.nc"
-filename = os.path.join(script_dir, fn)
-zoidberg.write_maps(grid, field, maps["1"], metric2d=False, gridfile=filename)
-gf = xr.open_dataset(filename)
-cellvolumes = (gf["J"] * gf["dx"] * gf["dy"] * gf["dz"]).values
-cellareas = (
-    (np.sqrt(gf["g_11"] * gf["g_33"] - gf["g_13"] * gf["g_13"])) * gf["dx"] * gf["dz"]
-)
-
-
-plotting = False
-
-if plotting:
-    testb = polyfunc(np.linspace(0.0, 2.0 * np.pi, 100), field.polynomials)
-    fig, ax = plt.subplots()
-    ax.plot(np.linspace(0.0, 2.0 * np.pi, 100), testb)
-    ax.set_ylabel("B [T]")
-    ax.set_xlabel(r"$\phi$ [rad]")
-    plt.show()
-
-    thisy = 0
-
-    fig, ax = plt.subplots()
-    a = ax.pcolormesh(
-        gf["R"][:, thisy, :], gf["Z"][:, thisy, :], cellvolumes[:, thisy, :]
-    )
-    plt.colorbar(a, label="J")
-
-# %% Create the numerical integration
-
-ycoord = np.linspace(0, 2.0 * np.pi, ny, endpoint=False)
-
-thisx = 5
-thisy = 3
-thisz = 3
-n = 500
-
-grid_dl = np.sqrt(gf["g_22"]) * gf["dy"]
-
-thisycoord = np.linspace(
-    (ycoord[thisy] + ycoord[thisy - 1]) / 2.0,
-    (ycoord[thisy] + ycoord[thisy + 1]) / 2.0,
-    n,
-)
-
-result = 0.0
-arclength = 0.0
-for i in range(n - 1):
-    thisfield = 0.5 * (
-        field.Byfunc(0.0, 0.0, thisycoord[i])
-        + field.Byfunc(0.0, 0.0, thisycoord[i + 1])
-    )
-    dl = (
-        (thisycoord[i + 1] - thisycoord[i])
-        / (2.0 * np.pi)
-        * (gf["R"][thisx, thisy, thisz] * 2.0 * np.pi)
-    )
-    arclength += dl.values
-    result += (
-        cellareas[thisx, thisy, thisz]
-        * field.Byfunc(0.0, 0.0, ycoord[thisy])
-        / thisfield
-        * dl
-    )
-
-
-# %% DO the comparison
-
-cellvolumes = {}
-error = {}
-errors = []
-for ref in reffactors:
-    cellvolumes[str(ref)] = (
-        maps[str(ref)]["J"] * gf["dx"] * gf["dy"] * gf["dz"]
-    ).values
-    error[str(ref)] = abs(cellvolumes[str(ref)][thisx, thisy, thisz] - result)
-    errors.append(error[str(ref)])
-
-coeffs = np.polyfit(np.log(reffactors), np.log(errors), 1)
-a, b = coeffs
-print("Convergence:", -a)
-
-if plotting:
-    fig, ax = plt.subplots()
+    reffactors = [1, 5, 10, 20, 50]
+    
+    maps = {}
     for ref in reffactors:
-        ax.scatter(ref, error[str(ref)])
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    ax.grid(True)
-    ax.set_xlabel("Refinement factor")
-    ax.set_ylabel("Error")
-    plt.show()
+        maps[str(ref)] = zoidberg.make_maps(grid, field, refine_parallel_integral=ref)
+    
+    
+    fn = f"testslabgrid_{nx}_{ny}_{nz}.nc"
+    filename = os.path.join(script_dir, fn)
+    zoidberg.write_maps(grid, field, maps["1"], metric2d=False, gridfile=filename)
+    gf = xr.open_dataset(filename)
+    cellvolumes = (gf["J"] * gf["dx"] * gf["dy"] * gf["dz"]).values
+    cellareas = (
+        (np.sqrt(gf["g_11"] * gf["g_33"] - gf["g_13"] * gf["g_13"])) * gf["dx"] * gf["dz"]
+    )
+    
+    
+    plotting = False
+    
+    if plotting:
+        testb = polyfunc(np.linspace(0.0, 2.0 * np.pi, 100), field.polynomials)
+        fig, ax = plt.subplots()
+        ax.plot(np.linspace(0.0, 2.0 * np.pi, 100), testb)
+        ax.set_ylabel("B [T]")
+        ax.set_xlabel(r"$\phi$ [rad]")
+        plt.show()
+    
+        thisy = 0
+    
+        fig, ax = plt.subplots()
+        a = ax.pcolormesh(
+            gf["R"][:, thisy, :], gf["Z"][:, thisy, :], cellvolumes[:, thisy, :]
+        )
+        plt.colorbar(a, label="J")
+    
+    
+    
+    ycoord = np.linspace(0, 2.0 * np.pi, ny, endpoint=False)
+    
+    thisx = 5
+    thisy = 3
+    thisz = 3
+    n = 500
 
-fail = False
-
-if abs(a) < 1.8:
-    fail = True
-
-assert not fail
+    
+    thisycoord = np.linspace(
+        (ycoord[thisy] + ycoord[thisy - 1]) / 2.0,
+        (ycoord[thisy] + ycoord[thisy + 1]) / 2.0,
+        n,
+    )
+    
+    result = 0.0
+    arclength = 0.0
+    for i in range(n - 1):
+        thisfield = 0.5 * (
+            field.Byfunc(0.0, 0.0, thisycoord[i])
+            + field.Byfunc(0.0, 0.0, thisycoord[i + 1])
+        )
+        dl = (
+            (thisycoord[i + 1] - thisycoord[i])
+            / (2.0 * np.pi)
+            * (gf["R"][thisx, thisy, thisz] * 2.0 * np.pi)
+        )
+        arclength += dl.values
+        result += (
+            cellareas[thisx, thisy, thisz]
+            * field.Byfunc(0.0, 0.0, ycoord[thisy])
+            / thisfield
+            * dl
+        )
+    
+    
+    cellvolumes = {}
+    error = {}
+    errors = []
+    for ref in reffactors:
+        cellvolumes[str(ref)] = (
+            maps[str(ref)]["J"] * gf["dx"] * gf["dy"] * gf["dz"]
+        ).values
+        error[str(ref)] = abs(cellvolumes[str(ref)][thisx, thisy, thisz] - result)
+        errors.append(error[str(ref)])
+    
+    coeffs = np.polyfit(np.log(reffactors), np.log(errors), 1)
+    a, b = coeffs
+    print("Convergence:", -a)
+    
+    if plotting:
+        fig, ax = plt.subplots()
+        for ref in reffactors:
+            ax.scatter(ref, error[str(ref)])
+        ax.set_xscale("log")
+        ax.set_yscale("log")
+        ax.grid(True)
+        ax.set_xlabel("Refinement factor")
+        ax.set_ylabel("Error")
+        plt.show()
+    
+    fail = False
+    
+    if abs(a) < 1.8:
+        fail = True
+    
+    assert not fail
